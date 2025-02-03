9786cec9c3e6d2a559a48f8bfdaf5d6023a89e14
Yogesh Ramsorrun <specineff00@gmail.com>
2025-02-03 18:42:03 +0000

    Initial Commit

---
+++ MomentumKit/MomentumKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumKitApp.swift
+//  MomentumKit
+//
+//  Created by Yogesh N Ramsorrun on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct MomentumKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MomentumKitTests/MomentumKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  MomentumKitTests.swift
+//  MomentumKitTests
+//
+//  Created by Yogesh N Ramsorrun on 03/02/2025.
+//
+
+import Testing
+@testable import MomentumKit
+
+struct MomentumKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
