0ff64fb861ea193a1a7ddadcb1f57d556740c825
Yogesh Ramsorrun <specineff00@gmail.com>
2025-02-19 11:38:20 +0000

    Request Fixed to use Content Type and headers

--- MomentumKit/Networking/Request.swift
+++ MomentumKit/Networking/Request.swift
@@ -1,4 +1,3 @@
-import Alamofire
 import Foundation
 
 protocol Request {
@@ -21,13 +20,15 @@ extension Request {
 
 extension Request {
   private func requestBodyFrom(params: Codable?) -> Data? {
-    guard let params = params else { return nil }
+    guard let params else { return nil }
     let encoder = JSONEncoder()
+    encoder.dateEncodingStrategy = .iso8601
     encoder.outputFormatting = .prettyPrinted
 
     guard let httpBody = try? encoder.encode(params) else {
       return nil
     }
+
     return httpBody
   }
 
@@ -39,7 +40,11 @@ extension Request {
     var request = URLRequest(url: finalURL)
     request.httpMethod = method.rawValue
     request.httpBody = requestBodyFrom(params: body)
-    request.allHTTPHeaderFields = headers
+
+    var finalHeaders = ["Content-Type": contentType]
+    headers.map { finalHeaders.merge($0) { (_, new) in new } }
+
+    request.allHTTPHeaderFields = finalHeaders
     return request
   }
 }
